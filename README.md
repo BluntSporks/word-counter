44f51d5aee91db6c7c3060078f1f77e1100546bc
Douglas Green <bluntsporks+git@gmail.com>
2015-05-07 05:11:47 -0400

    Adding lookup function

--- README.md
+++ README.md
@@ -12,7 +12,23 @@ Results are sorted in descending order by frequency.
 Ready to use
 
 ## Installation
-Use go install.
+This program is written in Google Go language. Make sure that Go is installed and the GOPATH is set up as described in
+[How to Write Go Code](https://golang.org/doc/code.html).
+
+The install this program and its dependencies by running:
+
+    go get github.com/BluntSporks/word-counter
 
 ## Usage
-word-counter [filename]
+There are two modes of usage:
+* Count all the words in a file.
+* Look up the word counts of words in a file using an existing word count file.
+
+Usage:
+
+    word-counter FILENAME
+    word-counter -lookup=LOOKUPFILE FILENAME
+
+Options:
+
+    -lookup=LOOKUPFILE  Existing word count to use for lookup
--- word-counter.go
+++ word-counter.go
@@ -3,11 +3,13 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"github.com/BluntSporks/mapsort"
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -15,10 +17,29 @@ var wordRegExp = regexp.MustCompile(`\pL+('\pL+)*`)
 var wordCnts = make(map[string]int)
 
 func main() {
-	if len(os.Args) < 2 {
+	lookup := flag.String("lookup", "", "Existing word count file to use for lookup")
+	flag.Parse()
+	if len(flag.Args()) < 1 {
 		log.Fatal("Missing filename argument")
 	}
-	file := os.Args[1]
+	file := flag.Arg(0)
+
+	// Choose program function.
+	if *lookup != "" {
+		lookUpCnts(file, *lookup)
+	} else {
+		cntWords(file)
+	}
+
+	// Print the word counts.
+	pairs := mapsort.ByVal(wordCnts, false)
+	for _, pair := range pairs {
+		fmt.Printf("%s,%d\n", pair.Key, pair.Val)
+	}
+}
+
+// cntWords counts the words in a file.
+func cntWords(file string) {
 	hdl, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
@@ -35,9 +56,48 @@ func main() {
 			wordCnts[word]++
 		}
 	}
-	// Print the substring counts.
-	pairs := mapsort.ByVal(wordCnts, false)
-	for _, pair := range pairs {
-		fmt.Printf("%s,%d\n", pair.Key, pair.Val)
+}
+
+// lookUpCnts looks up the word counts for a file in an existing word count file.
+func lookUpCnts(file string, lookup string) {
+	hdl, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer hdl.Close()
+	scanner := bufio.NewScanner(hdl)
+	wordsToLookUp := make(map[string]bool)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		line := strings.ToLower(scanner.Text())
+		words := wordRegExp.FindAllString(line, -1)
+		for _, word := range words {
+			wordsToLookUp[word] = true
+		}
+	}
+	lookupHdl, err := os.Open(lookup)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lookupHdl.Close()
+	scanner = bufio.NewScanner(lookupHdl)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		line := strings.ToLower(scanner.Text())
+		fields := strings.Split(line, ",")
+		if len(fields) != 2 {
+			continue
+		}
+		word := fields[0]
+		if wordsToLookUp[word] {
+			cnt, err := strconv.Atoi(fields[1])
+			if err == nil {
+				wordCnts[word] = cnt
+			}
+		}
 	}
 }
